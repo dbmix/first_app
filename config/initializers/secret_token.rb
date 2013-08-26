2382d1ad9e202332311407d871f4b6d82ef8870e
dbmix <db1mix@gmail.com>
2013-08-26 15:10:16 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '95e1ab6604582628229318a48f976186527c0e753bc205d0c275cc311e358bb093ae2ef7674cda879cc9a1dbcdcd2b0a8e0be2375a5b2f2c7b4434b1c7388c13'
